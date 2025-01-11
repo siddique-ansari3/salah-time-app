f24693dd83571ba8358433f2cbf9c2ed2d99dc1c
Siddique Ansari <Siddique>
2025-01-11 11:25:39 +0530

    android app files.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'salah-time-app',
+  webDir: 'dist/salah-time-app/browser',
+};
+
+export default config;
